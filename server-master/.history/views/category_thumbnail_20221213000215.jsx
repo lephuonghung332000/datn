76e14a69d43f75fc277d73d2d0ae3a21bbc96792
Hung Le <lephuonghung332000@gmail.com>
2022-12-13 13:16:32 +0700

    deploy fixfix

--- server-master/.history/views/category_thumbnail_20221213000215.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-const Image = (props) => {
-  const url = `${props.record.params["thumbnail"]}`;
-  return <img src={url} width={150} height={150} />
-};
-
-export default Image;
